d334f322ab7c570bb47450bdbe4309d0537d04a0
Baratali Haidari <baratalihaidari388@gmail.com>
2026-07-25 23:24:59 +0500

    feat(hero): implement responsive Hero section for TaskFlow

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from './components/Nav-Bar'
-import Home from "./components/Home";
-import Order from "./components/Order";
-import About from "./components/About";
-import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 import './App.css'
 
 function App() {
@@ -13,10 +10,8 @@ function App() {
     <BrowserRouter>
     <NavBar/>
     <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/About' element={<About/>} />
-      <Route path='/Order' element={<Order/>} />
-      <Route path='/Footer' element={<Footer/>} />
+        <Route path='/' element={<Hero/>} />
+
     </Routes>
     </BrowserRouter>
 
