be86fba262590a1ed4fb73295dec78aaabef19d8
Baratali Haidari <baratalihaidari388@gmail.com>
2026-07-28 12:52:44 +0500

    chore: install lucide-react package

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
     <NavBar/>
     <Routes>
         <Route path='/' element={<Hero/>} />
+       
 
     </Routes>
     </BrowserRouter>
--- src/components/Nav-Bar.jsx
+++ src/components/Nav-Bar.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 
 function NavBar(){
     return(
+    
         <ul className="flex justify-center space-x-5 bg-black p-4">
             <li><Link to='/' className="text-white font-bold uppercase hover:text-yellow-300 transition duration-300 
                      relative after:content-[''] after:block after:h-[1px] after:bg-orange-400 
@@ -16,6 +17,7 @@ function NavBar(){
                      relative after:content-[''] after:block after:h-[1px] after:bg-orange-400 
                      after:scale-x-0 hover:after:scale-x-100 after:transition after:duration-300 after:origin-center">Footer</Link></li>
         </ul>
+
     )
 }
 export default NavBar;
