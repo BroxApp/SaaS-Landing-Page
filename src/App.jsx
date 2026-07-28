7d1d41ea2c0f9a0f02235b7ef3b75681567ccdf6
Baratali Haidari <baratalihaidari388@gmail.com>
2026-07-28 22:33:55 +0500

    feat: complete Features section with responsive Tailwind styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from './components/Nav-Bar'
 import Hero from "./components/Hero";
+import Features from "./components/FeaturesGrid";
+
 import './App.css'
 
 function App() {
@@ -11,7 +13,8 @@ function App() {
     <NavBar/>
     <Routes>
         <Route path='/' element={<Hero/>} />
-       
+        <Route path='/Features' element={<Features/>} />
+
 
     </Routes>
     </BrowserRouter>
