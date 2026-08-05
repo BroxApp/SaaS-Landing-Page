4364ae5a3d314af609df1b3cac75ffe8312a5a8e
Baratali Haidari <baratalihaidari388@gmail.com>
2026-08-05 08:05:57 +0500

    added pricing siction

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from './components/Nav-Bar'
 import Hero from "./components/Hero";
 import Features from "./components/FeaturesGrid";
 import PricingSection from "./components/Pricing-Section";
+import Footer from "./components/Footer";
 
 import './App.css'
 
@@ -16,6 +17,7 @@ function App() {
         <Route path='/' element={<Hero/>} />
         <Route path='/Features' element={<Features/>} />
         <Route path='/PricingSection' element={<PricingSection/>} />
+        <Route path='/Footer' element={<Footer/>} />
         
 
     </Routes>
